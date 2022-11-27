db45280158f49981f143377a650c81e5f14a8252
Konstantin <kaplan.sk8@mail.ru>
2022-11-27 22:29:23 +0300

    upd
    comments deleted

--- src/firebase/init_firebase.js
+++ src/firebase/init_firebase.js
@@ -1,5 +1,4 @@
-//import { getAuth } from "firebase/auth";
-import { getFirestore, initializeFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
@@ -14,18 +13,5 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 
-// const firestoreDB = initializeFirestore(app, {
-// 	experimentalForceLongPolling: true,
-// });
-
-// apiKey: "AIzaSyBky1jZI5DhFUGjkhRLaFWJ4yOc2IdnLEw",
-// 	authDomain: "task-list-c611a.firebaseapp.com",
-// 	projectId: "task-list-c611a",
-// 	storageBucket: "task-list-c611a.appspot.com",
-// 	messagingSenderId: "734335373973",
-// 	appId: "1:734335373973:web:e5dd06735c8108fce2019c",
-// 	measurementId: "G-841RVDDZPS",
-
-//export const auth = getAuth();
 export const firestore = getFirestore();
 export const storage = getStorage(app);
